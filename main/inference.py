841a52773c5dc0a6033f39c8e433d28833e35031
Gleb Skiba <gleb-skiba@mail.ru>
2023-02-24 23:31:54 +0300

    Create inference signature

--- main/inference.py
+++ main/inference.py
@@ -2,5 +2,7 @@
 
 from main_utils import create_tensor_from_trajectory_point, create_shift_tesor
 
-def inference(model, start_point, shift, eps = 0.01):
-    start_point = 
+def inference(model, start_point, shift):
+    start_point = create_tensor_from_trajectory_point(start_point)
+    shift = create_shift_tesor(shift)
+    pass
